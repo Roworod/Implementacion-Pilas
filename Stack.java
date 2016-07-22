fd70a7e5c795f28f6b8fc29bd2078b615c45940c
roworo rod <roworo rod>
2016-07-22 13:19:37 -0600

    Stack v1.0

---
+++ Stack.java
@@ -0,0 +1,28 @@
+/************************
+*Stack
+*@version: 1.0
+*@author: Robbin Woods 15201
+*****************************/
+
+public class Stack{
+	private String[] vector.
+
+	public void setVector(String vector){
+
+	}
+
+	public String getVector(){
+
+	}
+
+	public int size(){}
+
+	public int setSize(int size){
+		vector= String[int];
+	} 
+
+	public void push(Object e){
+
+	}
+
+}
