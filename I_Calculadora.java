0d18b818c133916a8e31649c98283c4d07cccde8
roworo rod <roworo rod>
2016-07-21 18:57:52 -0600

    lleva interfaces v1.0

---
+++ I_Calculadora.java
@@ -0,0 +1,12 @@
+/************************
+*interfaz de  Pila
+*@version: 1.0
+*@author: Robbin Woods 15201
+*****************************/
+
+public interface I_Calculadora{
+	public int calcular(String vector);
+	public String toString();
+	public String leerArchivo(String direccion);
+	
+}
---
+++ I_Stack.java
@@ -0,0 +1,14 @@
+/************************
+*interfaz de  Pila
+*@version: 1.0
+*@author: Robbin Woods 15201
+*****************************/
+
+public interface I_Stack{
+
+	public void push(Object t);
+	public Object pop();
+	public boolean isEmpty();
+	public int size();
+	public Object get();
+}
