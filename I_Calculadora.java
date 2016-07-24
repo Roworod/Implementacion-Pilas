261f72d9727f68eebb41da461ecd39555da397ac
HiramRV <rub15044@uvg.edu.gt>
2016-07-24 10:30:12 -0600

    Update I_Calculadora.java

--- I_Calculadora.java
+++ I_Calculadora.java
@@ -1,8 +1,10 @@
-/************************
-*interfaz de  Pila
-*@version: 1.0
-*@author: Robbin Woods 15201
-*****************************/
+/**
+*I_Calculadora, Esta define los metodos que seran implementados en la calculadora
+*@version: 3.0
+*@author: Robbin Woods 15201, Steven Rubio 15044, Erick Bautista 15192
+*@since 2016-07-24
+*/
+
 
 public interface I_Calculadora{
 	public int calcular(String vector);
