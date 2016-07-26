c4472a2bc3cd0b1d43075f4e1850b88162a459f4
bau192 <bau15192@uvg.edu.gt>
2016-07-25 21:26:55 -0600

    I_Stack final, con los pre y post
    se agregaron pre y post como lo indicaba la documentacion de HDT2

--- I_Stack.java
+++ I_Stack.java
@@ -9,8 +9,14 @@
 public interface I_Stack{
 
 	public void push(Object t);
+	// post: agrega  un elemento a la pila
 	public Object pop();
+	// pre: hay un elemento en la pila
+	// post: Carga el ultimo elemento de la pila y lo elemina de la pila
 	public boolean isEmpty();
+	// post: retorna si la lista esta vacia o no
 	public int size();
+	// post: retorna el tamaño de la pila
 	public Object get();
+	// post: almacena el valor de un objeto de la pila 
 }
