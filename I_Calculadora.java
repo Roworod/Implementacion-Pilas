b01b14febecbc9aa62b2a6e98ae9189785ccf6a5
bau192 <bau15192@uvg.edu.gt>
2016-07-25 21:28:38 -0600

    I_calculadora final, con pre y post
    I_calculadora con documentacion pre y post

--- I_Calculadora.java
+++ I_Calculadora.java
@@ -8,7 +8,11 @@
 
 public interface I_Calculadora{
 	public int calcular(String vector);
+	// pre: recibe el string con las intrucciones
+	// post: regresa el resultado de las operacaciones
 	public String toString();
+	// post: imprime la operacion que se realizo	
 	public String leerArchivo(String direccion);
-	
+	// pre:	Carga en un String la direccion de un Archivo
+	// post: retorna en un String la operacion a realizar
 }
